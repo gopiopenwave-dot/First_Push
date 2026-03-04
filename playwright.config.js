a0913608d20e0b278ecbca487b7de0d26e222646
gopiopenwave-dot <gopi.openwave@gmail.com>
2026-03-04 18:00:28 +0530

    Customer code add and retrie also added

--- playwright.config.js
+++ playwright.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   testDir: './tests',
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
 
 
   reporter:  [['html', { open: 'on-failure' }]],
